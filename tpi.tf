582c956c76ed883317a631aeb5508a240abd4186
Helio Machado <0x2b3bfa0+git@googlemail.com>
2021-11-29 18:47:53 +0100

    Create tpi.tf

---
+++ tpi.tf
@@ -0,0 +1,47 @@
+terraform {
+  required_providers {
+    iterative = {
+      source = "iterative/iterative",
+    }
+  }
+}
+
+provider "iterative" {}
+
+variable "name" {
+  default = "runner"
+}
+
+variable "parallelism" {
+  default = 1
+}
+
+resource "iterative_task" "runners" {
+  cloud       = "aws"
+  name        = var.name
+  parallelism = var.parallelism
+  directory   = "."
+
+  environment = {
+    "CML_*" = "",
+    "GITHUB_*" = "",
+    "GITLAB_*" = "",
+    "BITBUCKET_*" = "",
+    "REPO_TOKEN" = "",
+  }
+
+  script = <<-END
+    #!/bin/bash
+    export HOME=/root
+    
+    curl --silent https://download.docker.com/linux/static/stable/x86_64/docker-20.10.9.tgz |
+    tar --extract --strip-components=1 --directory=/usr/bin
+    dockerd &
+    
+    curl --silent --location https://deb.nodesource.com/setup_14.x | bash
+    apt update
+    apt install --yes build-essential git nodejs
+    npm install --global --unsafe @dvcorg/cml
+    cml runner
+  END
+}
